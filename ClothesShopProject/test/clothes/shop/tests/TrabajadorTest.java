b3472493b4c808684c2ecdb9cbf66affa613fab2
Beñat Cristobal <b.cristobal@opendeusto.es>
2022-11-01 18:29:07 +0100

    Merge branch 'master' of git@github.com:bcristobal/ClothesShop-Project.git

--- ClothesShopProject/test/clothes/shop/tests/TrabajadorTest.java
+++ ClothesShopProject/test/clothes/shop/tests/TrabajadorTest.java
@@ -6,51 +6,78 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import clothes.shop.clases.Puesto;
+import clothes.shop.clases.Trabajador;
+
 public class TrabajadorTest {
 	
-	
+	private Trabajador trabajador;
+	private int id = 0;
+	private String nombre = "Nombre";
+	private String apellido = "Apellido";
+	private int sueldo = 1000;
+	private Puesto puesto = Puesto.EMPLEADO;
 
 	@Before
 	public void setUp() throws Exception {
+		trabajador = new Trabajador(id, nombre, apellido, sueldo, puesto);
+		
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		trabajador = null;
 	}
 
 	@Test
 	public void testToString() {
-		fail("Not yet implemented");
+		String toString = nombre + " " + apellido + ", " + sueldo + " €, " + puesto;
+		assertEquals(trabajador.toString(), toString);
 	}
 
 	@Test
 	public void testTrabajadorIntStringStringIntPuesto() {
-		fail("Not yet implemented");
+		assertNotNull(trabajador);
+		assertEquals(trabajador.getId(), id);
+		assertEquals(trabajador.getNombre(), nombre);
+		assertEquals(trabajador.getApellido(), apellido);
+		assertEquals(trabajador.getSueldo(), sueldo);
+		assertEquals(trabajador.getPuesto(), puesto);
 	}
 
 	@Test
 	public void testTrabajador() {
-		fail("Not yet implemented");
+		Trabajador trabajadorVacio = new Trabajador();
+		assertNotNull(trabajadorVacio);
+		assertEquals(trabajadorVacio.getId(), 0);
+		assertEquals(trabajadorVacio.getNombre(), null);
+		assertEquals(trabajadorVacio.getApellido(), null);
+		assertEquals(trabajadorVacio.getSueldo(), 1000);
+		assertEquals(trabajadorVacio.getPuesto(), Puesto.EMPLEADO);
 	}
 
 	@Test
 	public void testGetSueldo() {
-		fail("Not yet implemented");
+		assertEquals(trabajador.getSueldo(), sueldo);
 	}
 
 	@Test
 	public void testSetSueldo() {
-		fail("Not yet implemented");
+		int sueldoNuevo = 2000;
+		trabajador.setSueldo(sueldoNuevo);
+		assertEquals(sueldoNuevo, trabajador.getSueldo());
 	}
 
 	@Test
 	public void testGetPuesto() {
-		fail("Not yet implemented");
+		assertEquals(trabajador.getPuesto(), puesto);
 	}
 
 	@Test
 	public void testSetPuesto() {
-		fail("Not yet implemented");
+		Puesto puestoNuevo = Puesto.JEFE;
+		trabajador.setPuesto(puestoNuevo);
+		assertEquals(trabajador.getPuesto(), puestoNuevo);
 	}
 
 }
