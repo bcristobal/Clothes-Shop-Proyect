dd2e2f29e6bf73d5138fae60043240c0ca712899
Beñat Cristobal <b.cristobal@opendeusto.es>
2022-11-01 17:51:33 +0100

    Crear Trabajador test

---
+++ ClothesShopProject/test/clothes/shop/tests/TrabajadorTest.java
@@ -0,0 +1,56 @@
+package clothes.shop.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TrabajadorTest {
+	
+	
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTrabajadorIntStringStringIntPuesto() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTrabajador() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSueldo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetSueldo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPuesto() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPuesto() {
+		fail("Not yet implemented");
+	}
+
+}
