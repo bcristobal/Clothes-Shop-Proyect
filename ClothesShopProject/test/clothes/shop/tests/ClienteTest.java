3d4daf608a54135f3366899e3aac912da7c39fb4
Iker Ordoñez <iker.o.f@opendeusto.es>
2022-11-04 12:08:10 +0100

    Eliminar imports sobrantes

--- ClothesShopProject/test/clothes/shop/tests/ClienteTest.java
+++ ClothesShopProject/test/clothes/shop/tests/ClienteTest.java
@@ -5,8 +5,6 @@ import static org.junit.Assert.*;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.print.attribute.standard.PrinterInfo;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
