bd768b97753190382f17de41222ce817d915200b
MiguelAchaD <miguel.acha@opendeusto.es>
2023-01-17 20:12:34 +0100

    Funcionalidad VentanaPerfil

--- ClothesShopProject/src/clothes/shop/ventanas/VentanaPerfil.java
+++ ClothesShopProject/src/clothes/shop/ventanas/VentanaPerfil.java
@@ -1,88 +1,91 @@
 package clothes.shop.ventanas;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
-import javax.swing.BorderFactory;
-import javax.swing.BoxLayout;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class VentanaPerfil extends JFrame{
+import clothes.shop.clases.BaseDatos;
+import clothes.shop.clases.Cliente;
+import clothes.shop.clases.Trabajador;
 
+public class VentanaPerfil extends JFrame{
+	
+	String cookieUsuario = VentanaLOGGINN.getCookieUsuario();
     private static final long serialVersionUID = 1L;
     public VentanaPerfil() {
-   	 //VISUAL
-   	 JPanel Datos = new JPanel();
-   		 //TODO:Seguir la sesión (Quien es el usuario al venir a esta ventana) (comunicación entre ventanas) mediante pseudo-cookies
-   		 Datos.setLayout(new BoxLayout(Datos, BoxLayout.Y_AXIS));
-   		 Datos.add(new JLabel("Nombre:"));
-   		 //TODO:Rellenar datos con la base de datos (if...)
-   		 String nombreBaseDatos = "Nombre no figura en la base de datos";
-   		 JLabel Nombre = new JLabel(nombreBaseDatos);
-   		 Nombre.setForeground(Color.RED);
-   		 Datos.add(Nombre);
-   		 Datos.add(new JLabel("DNI/ID:"));
-   		 //TODO:Rellenar datos con la base de datos (if...)
-   		 String IDBaseDatos = "ID no figura en la base de datos";
-   		 JLabel ID = new JLabel(IDBaseDatos);
-   		 ID.setForeground(Color.RED);
-   		 Datos.add(ID);
-   		 Datos.add(new JLabel("Correo:"));
-   		 //TODO:Rellenar datos con la base de datos (if...)
-   		 String correoBaseDatos = "Correo no figura en la base de datos";
-   		 JLabel Correo = new JLabel(correoBaseDatos);
-   		 Correo.setForeground(Color.RED);
-   		 Datos.add(Correo);
-   	 
-   	 JPanel FotoUsuario = new JPanel();
-   		 JLabel imagen = new JLabel();
-   		 imagen.setIcon(new ImageIcon("foto/fotoPerfilPredeterminada.jpg"));
-   		 imagen.setPreferredSize(new Dimension( 200, 200 ));
-   		 FotoUsuario.add( imagen );
-   	 
-   	 JPanel ParteSuperior = new JPanel();
-   		 //TODO:ANYADIR LOGO
-   		 ParteSuperior.setLayout(new BorderLayout());
-   		 JButton hBoton = new JButton("←");
-   		 hBoton.setPreferredSize(new Dimension(44, 20));
-   		 ParteSuperior.add(hBoton, BorderLayout.WEST);
-   		 
-     Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
-   	 
-   	 Datos.setBorder(BorderFactory.createEmptyBorder(250,100,500,100));
-   	 FotoUsuario.setBorder(BorderFactory.createEmptyBorder(200,50,0,0));
-   	 ParteSuperior.setBorder(BorderFactory.createEmptyBorder(20,20,0,20));
-   	 
-   	 Container contentPane = getContentPane();
-   	 contentPane.add(Datos, BorderLayout.CENTER);
-   	 contentPane.add(FotoUsuario, BorderLayout.WEST);
-   	 contentPane.add(ParteSuperior, BorderLayout.NORTH);
+    	//SEGUIMIENTO DE LA BD
+    	Boolean tipo = true;
+    	//TODO: Crear diferentes VentanaPerfiles
+    	Trabajador t = new Trabajador();
+    	Cliente c = new Cliente();
+    	if (BaseDatos.existeTrabajador(cookieUsuario) == true) {
+    		tipo = true;
+			ArrayList<Trabajador> trabajadores = BaseDatos.getTrabajadores();
+			for (Trabajador trabajador : trabajadores) {
+				if (trabajador.getNombre().equals(cookieUsuario)) {
+					t.setNombre(trabajador.getNombre());
+					t.setApellido(trabajador.getApellido());
+					t.setId(trabajador.getId());
+					t.setPuesto(trabajador.getPuesto());
+				}
+			}
+		} else if (BaseDatos.existeCliente(cookieUsuario) == true) {
+			tipo = false;
+			ArrayList<Cliente> clientes = BaseDatos.getClientes();
+			for (Cliente cliente : clientes) {
+				if (cliente.getNombre().equals(cookieUsuario)) {
+					c.setNombre(cliente.getNombre());
+					c.setApellido(cliente.getApellido());
+					c.setId(cliente.getId());
+					c.setEsSocio(cliente.getEsSocio());
+				}
+			}
+		}
+    	
+    	//VISUAL
+    JPanel Datos = new JPanel();
+        
+    if (tipo == true) {
+    	JLabel perfilNombre = new JLabel(String.format("<html> <center> <br> <h4> Nombre y Apellido: %s %s </h4> </center> </html>", t.getNombre(), t.getApellido()));
+    	JLabel perfilId = new JLabel(String.format("<html> <center> <br> <h4> Id: %s </h4> </center> </html>", t.getId()));
+    	JLabel perfilPuesto = new JLabel(String.format("<html> <center> <br> <h4> Puesto: %s </h4> </center> </html>", t.getPuesto()));
+        Datos.add(perfilNombre);
+        Datos.add(perfilId);
+        Datos.add(perfilPuesto);
+	} else if (tipo == false) {
+		JLabel perfilNombre = new JLabel(String.format("<html> <center> <br> <h4> Nombre y Apellido: %s %s </h4> </center> </html>", c.getNombre(), c.getApellido()));
+		JLabel perfilId = new JLabel(String.format("<html> <center> <br> <h4> Id: %s </h4> </center> </html>", c.getId()));
+		JLabel perfilEsSocio = new JLabel(String.format("<html> <center> <br> <h4> Socio: %s </h4> </center> </html>", c.getEsSocio()));
+	    Datos.add(perfilNombre);
+	    Datos.add(perfilId);
+	    Datos.add(perfilEsSocio);
+	}
+    	
+    JPanel Header = new JPanel();
+    JLabel perfilHeader = new JLabel(String.format("<html> <center> <br> <h1> Welcome %s </h1> <h2> Personal Information </h2> </center> </html>", cookieUsuario));
+    Header.add(perfilHeader);
     
-   	 setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-   	 setSize(1000, 800);
-   	 int x = (int) ((dimension.getWidth() - getWidth()) / 2);
-   	 int y = (int) ((dimension.getHeight() - getHeight()) / 2);
-   	 setLocation(x, y);
-   	 setTitle("Perfil");
-   	 setVisible(true);
+    Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+    
+    Container contentPane = getContentPane();
+    contentPane.add(Header, BorderLayout.NORTH);
+    contentPane.add(Datos, BorderLayout.CENTER);
+    
+    setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    setSize(1000, 800);
+    int x = (int) ((dimension.getWidth() - getWidth()) / 2);
+    int y = (int) ((dimension.getHeight() - getHeight()) / 2);
+    setLocation(x, y);
+    setTitle("Perfil");
+    setVisible(true);
    	 
    	 //FUNCIONALIDAD
-   	 hBoton.addActionListener(new ActionListener() {    
-   		 @Override
-   		 public void actionPerformed(ActionEvent e) {
-   			 //TODO:Seguir la sesión (Quien es el usuario al mandarle a la otra ventana) (comunicación entre ventanas) mediante pseudo-cookies
-   			 VentanaPerfil.this.dispose();
-   			 VentanaPrincipal l = new VentanaPrincipal();
-   		 }
-   	 });
+   	 
     }
 }
